66d549f5bb34488fb69053ef00db554275786713
yourcarrot0214 <stylenbs@gmail.com>
2021-10-24 20:40:12 +0900

    5.2 todolist 스타일링 (1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import { NextPage } from "next";
-import styled from "styled-components";
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
 
-const Container = styled.div`
-  font-style: italic;
-  padding: 20px;
-`;
+const todos: TodoType[] = [
+  { id: 1, text: "리액트 공부하기", color: "red", checked: true },
+  { id: 2, text: "노드 공부하기", color: "orange", checked: true },
+  { id: 3, text: "넥스트 공부하기", color: "yellow", checked: false },
+  { id: 4, text: "타입스크립트 공부하기", color: "green", checked: false },
+  { id: 5, text: "개인 프로젝트 환경설정", color: "navy", checked: true },
+];
 
 const index: NextPage = () => {
-  return <Container>Hello Typescript!</Container>;
+  return <TodoList todos={todos} />;
 };
 
 export default index;
